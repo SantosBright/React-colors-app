5e21e1edfe5140f6f1fa8c281aa9835f9aa9c477
SantosBright <itzsantosbright15@gmail.com>
2020-01-11 23:59:36 +0100

    added palette and seedColors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="Palette">
+                {/* Navbar goes here */}
+                <div className="Palette-colors">{/* a bunch of color boxes */}</div>
+                <Palette {...seedColors[0]} />
             </div>
-        )
+        );
     }
 }
 
---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Palette</h1>
+            </div>
+        )
+    }
+}
+
+export default Palette;
