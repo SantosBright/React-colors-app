cf753c91dffbdfb95cd07f4586164f348e94a04a
Santos Bright <52054516+SantosBright@users.noreply.github.com>
2020-02-07 15:50:32 +0100

    start styling mini palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MinPalette from './MiniPalette';
 
 class PaletteList extends Component {
     render() {
@@ -8,9 +9,7 @@ class PaletteList extends Component {
             <div>
                 <h1>React Colors</h1>
                 {palettes.map(palette => (
-                    <p>
-                        <Link to={`/palette/${palette.id}`}>{palette.paletteName} </Link>
-                    </p>
+                    <MinPalette {...palette} />
                 ))}
             </div>
         )
--- src/seedColors.js
+++ src/seedColors.js
@@ -55,7 +55,7 @@ export default [
   {
     paletteName: "Flat UI Colors Dutch",
     id: "flat-ui-colors-dutch",
-    emoji: "🇳🇱",
+    emoji: "🎨",
     colors: [
       { name: "Sunflower", color: "#FFC312" },
       { name: "Energos", color: "#C4E538" },
@@ -82,7 +82,7 @@ export default [
   {
     paletteName: "Flat UI Colors American",
     id: "flat-ui-colors-american",
-    emoji: "🇺🇸",
+    emoji: "🤙",
     colors: [
       { name: "LightGreenishBlue", color: "#55efc4" },
       { name: "FadedPoster", color: "#81ecec" },
@@ -109,7 +109,7 @@ export default [
   {
     paletteName: "Flat UI Colors Aussie",
     id: "flat-ui-colors-aussie",
-    emoji: "🇦🇺",
+    emoji: "🎨",
     colors: [
       { name: "Beekeeper", color: "#f6e58d" },
       { name: "SpicedNectarine", color: "#ffbe76" },
@@ -136,7 +136,7 @@ export default [
   {
     paletteName: "Flat UI Colors British",
     id: "flat-ui-colors-british",
-    emoji: "🇬🇧",
+    emoji: "🤙",
     colors: [
       { name: "ProtossPylon", color: "#00a8ff" },
       { name: "Periwinkle", color: "#9c88ff" },
@@ -163,7 +163,7 @@ export default [
   {
     paletteName: "Flat UI Colors Spanish",
     id: "flat-ui-colors-spanish",
-    emoji: "🇪🇸",
+    emoji: "🎨",
     colors: [
       { name: "JacksonsPurple", color: "#40407a" },
       { name: "C64Purple", color: "#706fd3" },
@@ -190,7 +190,7 @@ export default [
   {
     paletteName: "Flat UI Colors Indian",
     id: "flat-ui-colors-indian",
-    emoji: "🇮🇳",
+    emoji: "🤙",
     colors: [
       { name: "OrchidOrange", color: "#FEA47F" },
       { name: "SpiroDiscoBall", color: "#25CCF7" },
@@ -217,7 +217,7 @@ export default [
   {
     paletteName: "Flat UI Colors French",
     id: "flat-ui-colors-french",
-    emoji: "🇫🇷",
+    emoji: "🎨",
     colors: [
       { name: "FlatFlesh", color: "#fad390" },
       { name: "MelonMelody", color: "#f8c291" },
