dab5603fbdcd4b78aee7263ad6c864ee194f3e0d
Santos Bright <52054516+SantosBright@users.noreply.github.com>
2020-03-03 14:22:11 +0100

    make ColorBox more responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,18 +15,17 @@ export default {
         },
         [sizes.down('lg')]: {
             width: '25%',
-            height: props => props.showFullPalette ? '20%' : '10%',
-            // marginBottom: '-0.255rem'
+            height: props => props.showFullPalette ? '20%' : '33.3333%',
         },
         [sizes.down('md')]: {
             width: '50%',
-            height: props => props.showFullPalette ? '10%' : '10%',
+            height: props => props.showFullPalette ? '10%' : '20%',
             // marginBottom: '-0.255rem'
         },
         [sizes.down('xs')]: {
             width: '100%',
             height: props => props.showFullPalette ? '5%' : '10%',
-            // marginBottom: '-0.255rem'
+            marginBottom: props => props.showFullPalette ? '-0.256rem' : '-0.289rem'
         }
     },
     copyText: {
@@ -126,6 +125,9 @@ export default {
             marginBottom: '0',
             padding: '1rem',
             textTransform: 'uppercase',
+            [sizes.down('xs')]: {
+                fontSize: '4.5rem'
+            }
         },
         '& p': {
             fontSize: '2rem',
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default {
     Palette: {
         height: '100vh',
@@ -15,7 +17,7 @@ export default {
         display: 'inline-block',
         position: 'relative',
         cursor: 'pointer',
-        marginBottom: '-5.8px',
+        marginBottom: '-0.25rem',
         backgroundColor: 'black',
         '& a': {
             color: 'white',
@@ -37,6 +39,18 @@ export default {
             textTransform: 'uppercase',
             transition: 'opacity .3s ease-out',
             textDecoration: 'none'
+        },
+        [sizes.down('lg')]: {
+            width: '25%',
+            height: '33.3333%'
+        },
+        [sizes.down('md')]: {
+            width: '50%',
+            height: '20%'
+        },
+        [sizes.down('xs')]: {
+            width: '100%',
+            height: '10%'
         }
     }
 }
